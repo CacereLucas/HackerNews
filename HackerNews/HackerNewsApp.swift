9bff5e323387c9f18d5878cbbc8ed2bcdc64300a
Lucas L. Cacere <lcacere04@me.com>
2021-12-20 16:54:44 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Lucas on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
