93827fa5c349f7c0716e3ca9bd1dee1a2b5e113b
Lucas L. Cacere <lcacere04@me.com>
2021-12-21 15:46:36 +0100

    Final change

---
+++ HackerNews/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Lucas Cacere on 20/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts ) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationTitle("HackerNews")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
